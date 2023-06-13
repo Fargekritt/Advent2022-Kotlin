d9ba7e980e0bd0bb9eaa5572370031fd7396c243
Fargekritt <Lullinj98@gmail.com>
2023-06-13 08:57:33 +0200

    Day 4 Readfile, and Started partA

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,2 +1,18 @@
 package day4
 
+import java.io.File
+
+fun main() {
+    val text = readFile()
+    partA(text)
+
+}
+
+fun readFile(): String {
+    return File("day4/Test").bufferedReader().use { it.readText() }
+}
+
+fun partA(text: String) {
+    val lines: List<String> = text.split("/r/n")
+    println(lines)
+}
