22b34b88b713f8c8324b5f37b04aa4593a8d3b5d
Amund <lullinj98@gmail.com>
2023-06-14 00:03:47 +0200

    Day 4 complete

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -5,6 +5,7 @@ import java.io.File
 fun main() {
     val text = readFile()
     println(partA(text))
+    println(partB(text))
 
 }
 
@@ -13,10 +14,7 @@ fun readFile(): String {
 }
 
 fun partA(text: String): Int {
-    val pairs: List<List<List<Int>>> = text.split("\r\n")
-            .map { it.split(",") }
-            .map { pair -> pair.map { it.split("-") }
-                    .map { strings -> strings.map { it.toInt() } } }
+    val pairs: List<List<List<Int>>> = lists(text)
     var amount = 0
 
     for (pair in pairs) {
@@ -27,7 +25,32 @@ fun partA(text: String): Int {
     return amount
 }
 
+fun partB(text: String): Int{
+    val pairs: List<List<List<Int>>> = lists(text)
+    var amount = 0
+
+    for (pair in pairs) {
+        if (overlaps(pair)){
+            amount++
+        }
+    }
+    return amount
+}
+
+private fun lists(text: String): List<List<List<Int>>> {
+    val pairs: List<List<List<Int>>> = text.split("\r\n")
+            .map { it.split(",") }
+            .map { pair ->
+                pair.map { it.split("-") }
+                        .map { strings -> strings.map { it.toInt() } }
+            }
+    return pairs
+}
+
 fun fullyContains(pair: List<List<Int>>): Boolean{
     return (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) || (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1])
+}
 
+fun overlaps(pair: List<List<Int>>): Boolean{
+    return !(pair[0][0] > pair[1][1] || pair[1][0] > pair[0][1])
 }
