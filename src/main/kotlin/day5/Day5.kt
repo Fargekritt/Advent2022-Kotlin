45caa9955e51ca9674bb506133ec1b4e6c9beac1
Amund <lullinj98@gmail.com>
2023-06-14 08:41:56 +0200

    Day 5 partA testing

--- src/main/kotlin/day5/Day5.kt
+++ src/main/kotlin/day5/Day5.kt
@@ -1,2 +1,19 @@
 package day5
 
+import java.io.File
+
+fun main(){
+    val text = readFile()
+    val (stack, moves) = text.split("\r\n\r\n")
+    val test =  stack.split("\r\n").map { it.split(" ") }
+    println(test.size)
+    for (strings in test) {
+        println(strings.size)
+    }
+    println("=====")
+    println(moves)
+}
+
+fun readFile(): String{
+    return File("day5/test").bufferedReader().use { it.readText() }
+}
